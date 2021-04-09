a985ad63d7fb66a4113a16eb91a3bd426296b1aa
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-04-09 23:29:55 +0900

    feat: user resource

---
+++ app/Resources/UserResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource {
+
+    /**
+     * リソースを配列へ変換する
+     *
+     * @param  \Illuminate\Http\Request
+     * @return array
+     */
+    public function toArray($request) {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'permissions' => [
+                'admin'       => $this->perm_admin       == 1,
+                'exhibition'  => $this->perm_exhibition  == 1,
+                'general'     => $this->perm_general     == 1,
+                'reservation' => $this->perm_reservation == 1,
+                'teacher'     => $this->perm_teacher     == 1,
+            ],
+        ];
+    }
+}
