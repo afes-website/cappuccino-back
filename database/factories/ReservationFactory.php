ceb02b2c474a852a04b9fa6dda6014d88e152a21
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-04-12 23:06:26 +0900

    test: reservation factory

---
+++ database/factories/ReservationFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Reservation;
+use App\Models\Term;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ReservationFactory extends Factory {
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Reservation::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition() {
+        return [
+            'id'=>$this->faker->userName,
+            'people_count'=>$this->faker->numberBetween(1),
+            'name'=>$this->faker->name,
+            'term_id'=>Term::factory(),
+            'email'=>$this->faker->email,
+            'address'=>$this->faker->address,
+            'cellphone'=>$this->faker->phoneNumber
+        ];
+    }
+}
