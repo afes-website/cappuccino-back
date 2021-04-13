161418a1382d404ff2c2719cfc8b35d2771c01d5
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-04-13 14:55:41 +0900

    test: GuestTest

---
+++ tests/guest/GuestTest.php
@@ -0,0 +1,62 @@
+<?php
+namespace Tests\guest;
+
+use Tests\TestCase;
+
+/*
+ * guest/:get guest/$id:get
+ */
+
+use App\Models\Guest;
+use App\Models\Term;
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class GuestTest extends TestCase {
+    public function testGetAll() {
+        $term_count = 3;
+        $guest_count = 3;
+        $user = User::factory()->permission('executive')->create();
+
+        for ($i = 0; $i < $term_count; $i++) {
+            Guest::factory()->count($term_count)->for(Term::factory())->count($guest_count)->create();
+        }
+
+        $this->actingAs($user)->get('/guests');
+        $this->assertResponseOk();
+        $this->receiveJson();
+        $res = json_decode($this->response->getContent());
+        $this->assertCount($term_count * $guest_count, $res);
+    }
+
+    public function testShow() {
+        $user = User::factory()->permission('executive')->create();
+        $guest = Guest::factory()->create();
+
+        $this->actingAs($user)->get("/guests/$guest->id");
+        $this->assertResponseOk();
+        $this->receiveJson();
+
+        $this->seeJsonEquals([
+            'id' => $guest->id,
+            'entered_at' => $guest->entered_at,
+            'exited_at' => $guest->exited_at,
+            'exh_id' => $guest->exh_id,
+            'term' => [
+                'id' => $guest->term->id,
+                'enter_scheduled_time' => $guest->term->enter_scheduled_time->toIso8601ZuluString(),
+                'exit_scheduled_time' => $guest->term->exit_scheduled_time->toIso8601ZuluString(),
+                'guest_type' => $guest->term->guest_type
+            ]
+        ]);
+    }
+
+    public function testNotFound() {
+        $user = User::factory()->permission('executive')->create();
+        $id = Str::random(8);
+        Guest::factory()->create();
+        $this->actingAs($user)->get("guests/$id");
+
+        $this->assertResponseStatus(404);
+    }
+}
