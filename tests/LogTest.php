9f15c474f8940e112491a23b4d19f64f37b06146
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-04-13 15:22:58 +0900

    test: Log testData

---
+++ tests/LogTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace Tests;
+
+/*
+ * log/
+ */
+
+use App\Resources\GuestResource;
+use App\Models\ActivityLog;
+use App\Models\User;
+
+class LogTest extends TestCase {
+    public function testData() {
+        $user = User::factory()->permission('executive')->create();
+        $log = ActivityLog::factory()->create();
+
+        $this->actingAs($user)->get('/log');
+        $this->assertResponseOk();
+        $this->receiveJson();
+        $this->seeJsonEquals([
+            [
+                'id' => $log->id,
+                'timestamp' => $log->timestamp->toIso8601ZuluString(),
+                'exh_id' => $log->exhihibion->id,
+                'guest' => new GuestResource($user),
+                'log_type' => $log->log_type,
+            ],
+        ]);
+    }
+}
