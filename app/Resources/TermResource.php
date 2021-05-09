7433cca128931c2753017b93ddc3e62850ca7b08
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-05-09 10:47:54 +0900

    fix: return term id

--- app/Resources/TermResource.php
+++ app/Resources/TermResource.php
@@ -14,6 +14,7 @@ class TermResource extends JsonResource {
      */
     public function toArray($request) {
         return [
+            "id" => $this->id,
             "enter_scheduled_time" => $this->enter_scheduled_time->toIso8601String(),
             "exit_scheduled_time" => $this->exit_scheduled_time->toIso8601String(),
             "guest_type" => $this->guest_type
