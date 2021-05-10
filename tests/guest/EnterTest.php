b6ba820c76b54c3c6d6ff03590cb72bf37a028e6
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-05-10 22:59:39 +0900

    test: Entertest Docs

--- tests/guest/EnterTest.php
+++ tests/guest/EnterTest.php
@@ -15,6 +15,10 @@ use Faker\Provider\DateTime;
  */
 class EnterTest extends TestCase {
 
+    /**
+     * 展示入室のテスト
+     * Guest オブジェクトが返ってきている
+     */
     public function testEnter() {
         $user = User::factory()->permission('exhibition')->has(Exhibition::factory())->create();
         $guest = Guest::factory()->create();
@@ -26,6 +30,10 @@ class EnterTest extends TestCase {
         // TODO: Guest が返ってくるか
     }
 
+    /**
+     * Guest が存在しないとき
+     * GUEST_NOT_FOUND が返ってきている
+     */
     public function testGuestNotFound() {
         $user = User::factory()->permission('exhibition')->has(Exhibition::factory())->create();
         Guest::factory()->create();
@@ -54,6 +62,10 @@ class EnterTest extends TestCase {
         $this->assertEquals('GUEST_NOT_FOUND', $code);
     }
 
+    /**
+     * Guest が既に展示に入室しているとき
+     * GUEST_ALREADY_ENTERED が返ってきている
+     */
     public function testAlreadyEntered() {
         $user = User::factory()
             ->permission('exhibition')
@@ -77,6 +89,10 @@ class EnterTest extends TestCase {
         $this->assertEquals('GUEST_ALREADY_ENTERED', $code);
     }
 
+    /**
+     * 展示の滞在者数制限に達している
+     * PEOPLE_LIMIT_EXCEEDED が返ってきている
+     */
     public function testPeopleLimitExceeded() {
         $guest_count = 5;
         $exhibition = Exhibition::factory()
@@ -94,6 +110,10 @@ class EnterTest extends TestCase {
         $this->assertEquals('PEOPLE_LIMIT_EXCEEDED', $code);
     }
 
+    /**
+     * Guest が既に退場処理をしている
+     * GUEST_ALREADY_EXITED が返ってきている
+     */
     public function testAlreadyExited() {
         $user = User::factory()->permission('exhibition')->has(Exhibition::factory())->create();
         $executive_user = User::factory()->permission('executive')->create();
@@ -116,6 +136,10 @@ class EnterTest extends TestCase {
         $this->assertEquals('GUEST_ALREADY_EXITED', $code);
     }
 
+    /**
+     * Guest が退場予定時間を過ぎている
+     * EXIT_TIME_EXCEEDED が返ってきている
+     */
     public function testExitTimeExceeded() {
         $term = Term::factory()->afterPeriod();
         $user = User::factory()->permission('exhibition')->has(Exhibition::factory())->create();
@@ -131,6 +155,10 @@ class EnterTest extends TestCase {
         $this->assertEquals('EXIT_TIME_EXCEEDED', $code);
     }
 
+    /**
+     * Exhibition が存在しない
+     * EXHIBITION_NOT_FOUND が返ってきている
+     */
     public function testExhibitionNotFound() {
         $user = User::factory()->permission('exhibition')->create();
         $guest = Guest::factory()->create();
@@ -146,6 +174,14 @@ class EnterTest extends TestCase {
         $this->assertEquals('EXHIBITION_NOT_FOUND', $code);
     }
 
+//    public function testExhibitionParam() {
+//        //TODO: Admin の時は任意の展示を触れて、Exhibitionは自分のしか触れない
+//    }
+
+    /**
+     * 権限チェック
+     * executive, 権限なし のユーザーの実行時に 403 が返ってきている
+     */
     public function testForbidden() {
         $users[] = User::factory()->permission('executive')->create();
         $users[] = User::factory()->create();
@@ -156,6 +192,10 @@ class EnterTest extends TestCase {
         }
     }
 
+    /**
+     * ログインチェック
+     * ログインしていないときに 403 が返ってきている
+     */
     public function testGuest() {
         $guest_id = Guest::factory()->create()->id;
 
