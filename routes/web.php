879bedde536b379d8d2ae6a77efdb15c2ca7652e
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-06-15 00:10:24 +0900

    style: add space after ","

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ $router->get('/', function () use ($router) {
 
 $router->post(
     '/auth/login',
-    ['uses'=>'AuthController@authenticate','middleware'=>'throttle:5,1']
+    ['uses'=>'AuthController@authenticate','middleware'=>'throttle:5, 1']
 ); // throttled 5 requests/1 min
 
 $router->get('/auth/user', ['uses'=>'AuthController@userInfo', 'middleware'=>'auth']);
@@ -31,7 +31,10 @@ $router->group(['prefix' => 'reservations'], function () use ($router) {
     $router->post('/', ['uses' => 'ReservationController@create']);
     $router->get('search', ['uses' => 'ReservationController@index', 'middleware' => 'auth:reservation']);
     $router->get('{id}', ['uses' => 'ReservationController@show', 'middleware' => 'auth:reservation']);
-    $router->get('{id}/check', ['uses' => 'ReservationController@check', 'middleware' => 'auth:reservation,executive']);
+    $router->get(
+        '{id}/check',
+        ['uses' => 'ReservationController@check', 'middleware' => 'auth:reservation, executive']
+    );
 });
 
 $router->group(['prefix' => 'guests'], function () use ($router) {
@@ -39,21 +42,21 @@ $router->group(['prefix' => 'guests'], function () use ($router) {
     $router->post('check-in', ['uses' => 'GuestController@checkIn', 'middleware' => 'auth:executive']);
     $router->get('{id}', ['uses' => 'GuestController@show', 'middleware' => 'auth:executive']);
     $router->post('{id}/check-out', ['uses' => 'GuestController@checkOut', 'middleware' => 'auth:executive']);
-    $router->post('{id}/enter', ['uses' => 'ExhibitionController@enter', 'middleware' => 'auth:exhibition, admin']);
-    $router->post('{id}/exit', ['uses' => 'ExhibitionController@exit', 'middleware' => 'auth:exhibition, admin']);
+    $router->post('{id}/enter', ['uses' => 'GuestController@enter', 'middleware' => 'auth:exhibition, admin']);
+    $router->post('{id}/exit', ['uses' => 'GuestController@exit', 'middleware' => 'auth:exhibition, admin']);
 });
 
-$router->get('terms', ['uses' => 'TermController@index', 'middleware' => 'auth:executive,exhibition']);
+$router->get('terms', ['uses' => 'TermController@index', 'middleware' => 'auth:executive, exhibition']);
 
 $router->get(
     'log',
-    ['uses' => 'ActivityLogController@index', 'middleware' => 'auth:executive,exhibition,reservation']
+    ['uses' => 'ActivityLogController@index', 'middleware' => 'auth:executive, exhibition, reservation']
 );
 
 
 $router->group(['prefix' => 'exhibitions'], function () use ($router) {
     $router->get('{id}', ['uses' => 'ExhibitionController@show', 'middleware' => 'auth:exhibition']);
-    $router->get('/', ['uses' => 'ExhibitionController@index', 'middleware' => 'auth:exhibition,executive']);
+    $router->get('/', ['uses' => 'ExhibitionController@index', 'middleware' => 'auth:exhibition, executive']);
 });
 
 $router->options('{path:.*}', function () {
