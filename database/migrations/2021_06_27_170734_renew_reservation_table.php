a06364b16997497128683ebcfdbd0e47956d5480
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-06-27 20:30:00 +0900

    style: fix by phpcbf

--- database/migrations/2021_06_27_170734_renew_reservation_table.php
+++ database/migrations/2021_06_27_170734_renew_reservation_table.php
@@ -4,8 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RenewReservationTable extends Migration
-{
+class RenewReservationTable extends Migration {
+
     /**
      * Run the migrations.
      *
