3554d9502b13564a9acf8d758cb36b1c21ae43d5
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-06-28 16:29:43 +0900

    feat: to allow multiple people check in

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -41,7 +41,7 @@ class Reservation extends Model {
             return 'OUT_OF_RESERVATION_TIME';
         }
 
-        if ($this->guest()->exists()) {
+        if ($this->guest()->count() >= $this->member_all) {
             return 'ALREADY_ENTERED_RESERVATION';
         }
 
