8a128725d97a6b9850650b408e3fc15ca72b822f
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-07-17 19:01:24 +0900

    chore: uploaded -> attached

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -18,10 +18,10 @@ class ImageController extends Controller {
     }
 
     public function create(Request $request) {
-        if (!$request->hasFile('content')) abort(400, 'file is not uploaded');
+        if (!$request->hasFile('content')) abort(400, 'file is not attached');
         $file = $request->file('content');
         $mime_type = $file->getMimeType();
-        if (substr($mime_type, 0, 6) !== 'image/') abort(400, 'uploaded file is not an image');
+        if (substr($mime_type, 0, 6) !== 'image/') abort(400, 'attached file is not an image');
 
         $image = Image::imageCreate($file, $request->user()->id, $mime_type);
 
