74ade98c394fe6e8c8841944380b4010f35b07ba
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-07-18 22:39:36 +0900

    feat: Guest calculateParity method

--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -27,14 +27,19 @@ class Guest extends Model {
     const ID_LENGTH = 5;
     const VALID_FORMAT = '/\A[A-Za-z]{2}-[0-9A-Fa-f]{5}\Z/';
 
+    public static function calculateParity(string $id_sub): string {
+        $digits = [];
+        foreach (str_split($id_sub) as $char) {
+            $digits[] = hexdec($char);
+        }
+        $parity_digits = ($digits[0] + $digits[1] * 3 + $digits[2] + $digits[3] * 3) % 0x10;
+        return strtoupper(dechex($parity_digits));
+    }
+
     public static function validate(string $guest_id): bool {
         if (!preg_match(self::VALID_FORMAT, $guest_id)) return false;
-        $digits = [];
-        foreach (str_split(substr($guest_id, 3)) as $digit) {
-            $digits[] = hexdec($digit);
-        }
-        if (($digits[0] + $digits[1] * 3 + $digits[2] + $digits[3] * 3) % 0x10 !== $digits[4]) return false;
-        return true;
+        $guest_id = strtoupper($guest_id);
+        return self::calculateParity(substr($guest_id, 3, 4)) === $guest_id[-1];
     }
 
 
