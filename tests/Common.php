34fbc3edd9528a638a9002110d9573856ab643eb
ibuki2003 <main@fuwa.dev>
2021-08-05 23:06:32 +0900

    feat: update behavior of multipleArray to achieve consistency

--- tests/Common.php
+++ tests/Common.php
@@ -12,13 +12,12 @@ class Common {
      */
     public static function multipleArray(array $head, array ...$tail): Generator {
         if (count($tail) == 0) {
-            foreach ($head as $item) yield $item;
+            foreach ($head as $item) yield [$item];
             return;
         }
         foreach ($head as $item1) {
             foreach (self::multipleArray(...$tail) as $item2) {
-                if (is_array($item2)) yield [$item1, ...$item2];
-                else yield[$item1, $item2];
+                yield [$item1, ...$item2];
             }
         }
     }
