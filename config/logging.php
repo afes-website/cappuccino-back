ddf25286de6f1e2177480911c354f429ef49e388
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-10-26 18:00:57 +0900

    chore: change logs slack username to "Cappuccino Backend Log"

--- config/logging.php
+++ config/logging.php
@@ -56,7 +56,7 @@ return [
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Lumen Log',
+            'username' => 'Cappuccino Backend Log',
             'emoji' => ':boom:',
             'level' => 'notice',
         ],
