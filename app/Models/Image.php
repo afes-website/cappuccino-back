5b1553234a018f1086d6312f00b986875495b867
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-11-12 00:33:40 +0900

    feat: make small image square

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -29,11 +29,7 @@ class Image extends Model {
             $constraint->upsize();
         };
 
-        if ($content_medium->width() >= $content_small->height()) {
-            $content_small->widen(400, $upsize_callback);
-        } else {
-            $content_small->heighten(400, $upsize_callback);
-        }
+        $content_small->fit(400, 400, $upsize_callback);
         $content_medium->resize(1440, 1440, function ($constraint) {
             $constraint->upsize();
             $constraint->aspectRatio();
