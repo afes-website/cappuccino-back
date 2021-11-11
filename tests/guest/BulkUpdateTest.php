3cd13cab82da277ac17ac306bc2052fa3803d47a
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-11-11 09:51:38 +0900

    test: bulk test applied

--- tests/guest/BulkUpdateTest.php
+++ tests/guest/BulkUpdateTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Tests\guest;
 
+use App\Models\ActivityLogEntry;
 use App\Models\Exhibition;
 use Database\Factories\GuestFactory;
 use Faker\Factory;
@@ -22,5 +23,50 @@ class BulkUpdateTest extends TestCase {
         $this->actingAs($user)->json('post', '/guests/bulk-update', [[]]);
         $this->assertResponseOk();
         $this->seeJson([]);
+    }
+
+    public function testApplied() {
+        $user = User::factory()->has(Exhibition::factory())->permission('executive', 'exhibition')->create();
+        $reservation = Reservation::factory()->create();
+        $factory = Factory::create();
+        $this->actingAs($user)->json('post', '/guests/bulk-update', [
+            [
+                'command' => 'check-in',
+                'reservation_id' => $reservation->id,
+                'guest_id' => GuestFactory::createGuestId(),
+                'timestamp' => $factory->dateTime()->format("Y-m-d H:i:s"),
+            ],
+            [
+                'command' => 'enter',
+                'guest_id' => Guest::factory()->create()->id,
+                'timestamp' => $factory->dateTime()->format("Y-m-d H:i:s"),
+            ],
+            [
+                'command' => 'exit',
+                'guest_id' => Guest::factory()->create()->id,
+                'timestamp' => $factory->dateTime()->format("Y-m-d H:i:s"),
+            ],
+            [
+                'command' => 'check-out',
+                'guest_id' => Guest::factory()->create()->id,
+                'timestamp' => $factory->dateTime()->format("Y-m-d H:i:s"),
+            ],
+            [
+                'command' => 'register-spare',
+                'guest_id' => GuestFactory::createGuestId(),
+                'reservation_id' => $reservation->id,
+                'timestamp' => $factory->dateTime()->format("Y-m-d H:i:s"),
+            ],
+        ]);
+        $this->assertResponseOk();
+        $this->seeJsonEquals([
+            ['is_applied' => true, 'code' => null],
+            ['is_applied' => true, 'code' => null],
+            ['is_applied' => true, 'code' => null],
+            ['is_applied' => true, 'code' => null],
+            ['is_applied' => true, 'code' => null],
+        ]);
+        $this->assertCount(5, ActivityLogEntry::all()->where('verified', false));
+        $this->assertCount(5, Guest::all());
     }
 }
